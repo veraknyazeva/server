0ab7feab8f44774820de5da532f19ab84c9d7dcb
Vera Knyazeva <verashibanova@yandex.ru>
2024-06-04 13:34:33 +0500

    first commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static String localhost = "127.0.0.1";
+
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket(localhost, Server.port);
+             PrintWriter out = new
+                     PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("GET / HTTP/1.1\n" +
+                    "Host: netology.ru\n\n\n");
+            String resp = in.readLine();
+            System.
+                    out.println(resp);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static int port = 80;
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+        Socket clientSocket = serverSocket.accept(); // ждем подключения
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.println("New connection accepted");
+
+        final String name = in.readLine();
+
+        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+    }
+}
